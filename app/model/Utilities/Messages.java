b0b43432c74131916cf0b91e035795a8636580de
shuklv1 <vibhor.shukla@rsa.com>
2017-12-17 00:42:36 +0530

    Added terminate client to error.scala

---
+++ app/model/Utilities/Messages.java
@@ -0,0 +1,8 @@
+package model.Utilities;
+
+/**
+ * Created by shuklv1 on 16-Dec-17.
+ */
+public class Messages {
+    public static String ERROR_CLIENT_EXISTS="Client is already opened in another window! Reload 'localhost:9000' else close all other open windows for Impact Path Flow and try again.";
+}

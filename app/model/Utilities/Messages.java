d56f5ae75f9e3774c2cd67694d44b984c3b51221
vibhor shukla <vibhsheros@gmail.com>
2017-12-17 21:47:42 +0530

    Added styling for error message. (#11)
    Downloaded jquery3.2.1

--- app/model/Utilities/Messages.java
+++ app/model/Utilities/Messages.java
@@ -4,5 +4,5 @@ package model.Utilities;
  * Created by shuklv1 on 16-Dec-17.
  */
 public class Messages {
-    public static String ERROR_CLIENT_EXISTS="Client is already opened in another window! Reload 'localhost:9000' else close all other open windows for Impact Path Flow and try again.";
+    public static String ERROR_CLIENT_EXISTS="<h4>Client is already opened in another window!</h4><p>Try one of the following:<br><br>1)Close all other open windows of Impact Path Flow and try again.<br>2)Reload [ipaddress]:9000<br></p>";
 }
